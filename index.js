0d72244a1d29c2374c184d2036d05c75ddb33ffc
Tomasz Wajsman <tomasz.wajsman@outlook.com>
2020-07-20 11:43:15 +0200

    Added response code translation to human language

--- index.js
+++ index.js
@@ -8,7 +8,15 @@ class SamClient {
   }
 
   static responseCodeToMessage(responseCode) {
-
+    switch (responseCode) {
+      default: return 'Unknown code';
+      case 200: return 'Activities were downloaded';
+      case 201: return 'Activity was created';
+      case 204: return 'Operation was finished';
+      case 400: return 'Missing or bad request arguments';
+      case 404: return 'No activity with specified ID';
+      case 500: return 'Server error has occurred';
+    }
   }
 
   async getActivities() {
