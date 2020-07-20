3321c3bed8e4dd0d660824d95aec627bf68c820f
Tomasz Wajsman <tomasz.wajsman@outlook.com>
2020-07-20 13:37:35 +0200

    Removed translating the codes
    It will be done in the app, not in the client that is responsible for
    managing the activities

--- index.js
+++ index.js
@@ -7,22 +7,6 @@ class SamClient {
     this.url = apiUrl;
   }
 
-  static checkDetails(activityDetails) {
-
-  }
-
-  static responseCodeToMessage(responseCode) {
-    switch (responseCode) {
-      default: return 'Unknown code';
-      case 200: return 'Activities were downloaded';
-      case 201: return 'Activity was created';
-      case 204: return 'Operation was finished';
-      case 400: return 'Missing or bad request arguments';
-      case 404: return 'No activity with specified ID';
-      case 500: return 'Server error has occurred';
-    }
-  }
-
   async getActivities() {
     const response = await axios.get(`${this.url}/activities`);
     return response.data.activities;
