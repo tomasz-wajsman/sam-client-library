cf377788ce1ebe58873b13aeaae15c4c44f9ab7c
Tomasz Wajsman <tomasz.wajsman@outlook.com>
2020-07-20 11:43:01 +0200

    Added the class for the client

---
+++ index.js
@@ -0,0 +1,34 @@
+const axios = require('axios');
+
+require('./config.js').configure();
+
+class SamClient {
+  static checkDetails(activityDetails) {
+
+  }
+
+  static responseCodeToMessage(responseCode) {
+
+  }
+
+  async getActivities() {
+
+  }
+
+  async getActivity(activityID) {
+
+  }
+
+  async createActivity(activityDetails) {
+
+  }
+
+  async modifyActivity(activityID, activityDetails) {
+
+  }
+
+  async deleteActivity(activityID) {
+
+  }
+}
+module.exports = SamClient;
